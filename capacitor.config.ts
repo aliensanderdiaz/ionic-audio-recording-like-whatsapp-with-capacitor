b5f6121e8fc18a73611758b91a9c3ee9f9bc2370
Alexander Diaz <aliensanderdiaz@gmail.com>
2023-02-15 17:07:32 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'academyVoice',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
